42d413cbe6f86fae74ea7e24cd4500450c676f1c
ainokyto <aino.kyto@gmail.com>
2020-04-10 13:53:40 +0300

    created game grid

---
+++ notes.js
@@ -0,0 +1,9 @@
+//* BUILD GRID AND PLAYER SPACESHIP
+
+//* Set up grid
+
+//* Make one square player 
+
+//* Make player move on key event
+
+//* Make player stop on grid border
---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+function init () {
+
+  //* Grid variables
+
+  const grid = document.querySelector('.grid')
+  const cells = []
+  const width = 11
+  const cellCount = width * width
+  
+  //* Create grid dynamically
+
+  const playerPosition = 94
+  function createGrid(startingPosition) {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    cells[startingPosition].classList.add('spaceship')
+  }
+  createGrid(playerPosition)
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
