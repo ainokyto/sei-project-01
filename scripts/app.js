e4545d3c921d77a7f2c4ca7c3f5c5ceda294f28b
ainokyto <aino.kyto@gmail.com>
2020-04-11 21:44:29 +0300

    created invader movement logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,22 @@
 function init() {
-  // * Game Start
+  // * DOM ELEMENTS ----------------------------------------------------------
 
   const startBtn = document.querySelector('.start')
 
+  // * GAME VARIABLES ------------------------------------------------------
+  let direction = 1
+  const grid = document.querySelector('.grid')
+  const cells = []
+  const width = 11
+  const cellCount = width * width
+
+  //* // * START GAME --------------------------------------------------------
+
   function gameInit() {
 
-    //* Grid variables
+    // * CREATE GRID AND PLAYER SPACESHIP-------------------------------------
 
-    const grid = document.querySelector('.grid')
-    const cells = []
-    const width = 20
-    const cellCount = width * width
-
-    //* Game start variables
-
-    let playerPosition = 390
-    const invaderArray = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
-
-    // createInvader()
-
-    //* Create grid 
-
+    let playerPosition = 115
     function createGrid(startingPosition) {
       for (let i = 0; i < cellCount; i++) {
         const cell = document.createElement('div')
@@ -31,27 +27,68 @@ function init() {
       cells[startingPosition].classList.add('spaceship')
     }
 
-    //* Player spaceship movement
+    // * CREATE INVADERS -----------------------------------------------------
+    let invaderArray = [0, 1, 2, 3, 4, 5, 6, 7, 
+      11, 12, 13, 14, 15, 16, 17, 18, 
+      22, 23, 24, 25, 26, 27, 28, 29]
+
+    function createInvaders() {
+      invaderArray.forEach(invader =>
+        cells[invader].classList.add('invaders'))
+    }
+    //* INVADER MOVEMENT ------------------------------------------------------
+    // in the if statement, -1 moves left, width down
+
+    //* game starts with invaders moving right
+    //* function removeAliens - remove classes
+    //* function addAliens - based on the direction variable
+
+
+    setInterval(function moveInvaders() {
+      removeInvaders() 
+      if (invaderArray[0] % width === 3 && direction === 1) {
+        direction = width
+      } else if (invaderArray[0] % width === 3 && direction === width) {
+        direction = -1
+      } else if (invaderArray[0] % width === 0 && direction === -1 ) {
+        direction = width
+      } else if (invaderArray[0] % width === 0 && direction === width) {
+        direction = 1
+      // } else if (invaderArray[0] > width * width - width) {
+      //   gameOver()
+      }
+      addInvaders()
+    }, 300)
+
+    //* REMOVE INVADERS CLASS --------------------------------------------------
+    function removeInvaders () {
+      invaderArray.forEach(invader =>
+        cells[invader].classList.remove('invaders'))
+    }
+
+    //* ADD INVADERS CLASS ------------------------------------------------------
+    function addInvaders() {
+      invaderArray = invaderArray.map(a => a + direction)
+      invaderArray.forEach(invader => {
+        cells[invader].classList.add('invaders')
+      })
+    }
+
+    //* PLAYER SPACESHIP MOVEMENT ---------------------------------------------
 
     function handleKeyDown(event) {
       cells[playerPosition].classList.remove('spaceship')
       switch (event.keyCode) {
         case 39:
-          playerPosition < 399 ? playerPosition++ : playerPosition
+          playerPosition < 120 ? playerPosition++ : playerPosition
           break
         case 37:
-          playerPosition > 380 ? playerPosition-- : playerPosition
+          playerPosition > 110 ? playerPosition-- : playerPosition
           break
         default:
           playerPosition
       }
       cells[playerPosition].classList.add('spaceship')
-    }
-
-    //* Create invaders --- 
-    function createInvaders() {
-      invaderArray.forEach(invader =>
-        cells[invader].classList.add('invaders'))
     }
 
     createGrid(playerPosition)
@@ -62,10 +99,15 @@ function init() {
   
   function handleStartBtn() {
     gameInit()
+    startBtn.disabled = true // Try to look for a way to hide button after click!
+    //* start a timer and store in a variable
   }
-
+  
+  //* EVENT LISTENERS
+  
+  
   startBtn.addEventListener('click', handleStartBtn)
-
+  
 }
 
 window.addEventListener('DOMContentLoaded', init)
