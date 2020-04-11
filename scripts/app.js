3b78b060999543d78760cfe6ae0a85c8376ad3d4
ainokyto <aino.kyto@gmail.com>
2020-04-11 15:57:37 +0300

    created invaders and game init function

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,71 @@
-function init () {
+function init() {
+  // * Game Start
 
-  //* Grid variables
+  const startBtn = document.querySelector('.start')
 
-  const grid = document.querySelector('.grid')
-  const cells = []
-  const width = 20
-  const cellCount = width * width
-  
-  //* Create grid dynamically
+  function gameInit() {
 
-  let playerPosition = 390
-  function createGrid(startingPosition) {
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      cell.textContent = i
-      grid.appendChild(cell)
-      cells.push(cell)
+    //* Grid variables
+
+    const grid = document.querySelector('.grid')
+    const cells = []
+    const width = 20
+    const cellCount = width * width
+
+    //* Game start variables
+
+    let playerPosition = 390
+    const invaderArray = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
+
+    // createInvader()
+
+    //* Create grid 
+
+    function createGrid(startingPosition) {
+      for (let i = 0; i < cellCount; i++) {
+        const cell = document.createElement('div')
+        cell.textContent = i
+        grid.appendChild(cell)
+        cells.push(cell)
+      }
+      cells[startingPosition].classList.add('spaceship')
     }
-    cells[startingPosition].classList.add('spaceship')
+
+    //* Player spaceship movement
+
+    function handleKeyDown(event) {
+      cells[playerPosition].classList.remove('spaceship')
+      switch (event.keyCode) {
+        case 39:
+          playerPosition < 399 ? playerPosition++ : playerPosition
+          break
+        case 37:
+          playerPosition > 380 ? playerPosition-- : playerPosition
+          break
+        default:
+          playerPosition
+      }
+      cells[playerPosition].classList.add('spaceship')
+    }
+
+    //* Create invaders --- 
+    function createInvaders() {
+      invaderArray.forEach(invader =>
+        cells[invader].classList.add('invaders'))
+    }
+
+    createGrid(playerPosition)
+    createInvaders()
+    document.addEventListener('keydown', handleKeyDown)
+    
+  }
+  
+  function handleStartBtn() {
+    gameInit()
   }
 
-  //* Player spaceship movement
+  startBtn.addEventListener('click', handleStartBtn)
 
-  function handleKeyDown(event) {
-    cells[playerPosition].classList.remove('spaceship')
-    switch (event.keyCode) {
-      case 39:
-        playerPosition < 399 ? playerPosition++ : playerPosition
-        break
-      case 37:
-        playerPosition > 380 ? playerPosition-- : playerPosition
-        break
-      default:
-        playerPosition
-    }
-    cells[playerPosition].classList.add('spaceship')
-  }
-  
-
-
-  createGrid(playerPosition)
-  document.addEventListener('keydown', handleKeyDown)
 }
 
 window.addEventListener('DOMContentLoaded', init)
