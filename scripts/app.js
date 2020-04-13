75957b3517d0f4c90b118af542b0db6824d4b637
ainokyto <aino.kyto@gmail.com>
2020-04-13 14:11:06 +0300

    created game over function, ade timer stop at game over

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,16 @@ function init() {
 
   const startBtn = document.querySelector('.start')
   const resetBtn = document.querySelector('.reset')
+  const score = document.querySelector('.actualscore')
 
   // * GAME VARIABLES ------------------------------------------------------
   let direction = 1
+  let gameRunning = false
+  let invaderArray = [0, 1, 2, 3, 4, 5, 6, 7,
+    11, 12, 13, 14, 15, 16, 17, 18,
+    22, 23, 24, 25, 26, 27, 28, 29]
+  let playerPosition = 115
+  let timerId = null
   const grid = document.querySelector('.grid')
   const cells = []
   const width = 11
@@ -13,11 +20,10 @@ function init() {
 
   //* // * START GAME --------------------------------------------------------
 
-  function gameInit() { 
+  function gameInit() {
 
     // * CREATE GRID AND PLAYER SPACESHIP -------------------------------------
 
-    let playerPosition = 115
     function createGrid(startingPosition) {
       for (let i = 0; i < cellCount; i++) {
         const cell = document.createElement('div')
@@ -27,11 +33,7 @@ function init() {
       }
       cells[startingPosition].classList.add('spaceship')
     }
-
-    // * CREATE INVADERS -----------------------------------------------------
-    let invaderArray = [0, 1, 2, 3, 4, 5, 6, 7, 
-      11, 12, 13, 14, 15, 16, 17, 18, 
-      22, 23, 24, 25, 26, 27, 28, 29]
+    // * CREATE INVADERS ON GRID ----------------------------------------------
 
     function createInvaders() {
       invaderArray.forEach(invader =>
@@ -40,30 +42,29 @@ function init() {
     //* INVADER MOVEMENT ------------------------------------------------------
     // in the if statement, -1 moves left, width down
 
-    //* game starts with invaders moving right
-    //* function removeAliens - remove classes
-    //* function addAliens - based on the direction variable
+    // game starts with invaders moving right
+    // function removeAliens - remove classes
+    // function addAliens - based on the direction(1) variable
 
 
-    setInterval(function moveInvaders() {
-      removeInvaders() 
-      
+    function moveInvaders() {
+      removeInvaders()
       if (invaderArray[0] % width === 3 && direction === 1) {
         direction = width
       } else if (invaderArray[0] % width === 3 && direction === width) {
         direction = -1
-      } else if (invaderArray[0] % width === 0 && direction === -1 ) {
+      } else if (invaderArray[0] % width === 0 && direction === -1) {
         direction = width
       } else if (invaderArray[0] % width === 0 && direction === width) {
         direction = 1
       } else if (invaderArray[0] > width * width - width) {
-        //! Make gameOver() function
+        gameOver()
       }
       addInvaders()
-    }, 1000)
+    }
 
     //* REMOVE INVADERS CLASS --------------------------------------------------
-    function removeInvaders () {
+    function removeInvaders() {
       invaderArray.forEach(invader =>
         cells[invader].classList.remove('invaders'))
     }
@@ -76,6 +77,19 @@ function init() {
       })
     }
 
+    //* START GAME TIMER
+    
+    function startTimer () {
+      // GLOBAL SCOPE let gameRunning = true // set a variable true when you create the premise,
+      if (!gameRunning) { 
+        timerId = setInterval(moveInvaders, 1000) 
+        gameRunning = true //have completion of the premise set the variable false,
+      } else {
+        gameRunning = false  // don't create another instance of the premise if the variable is true.
+      } 
+    }
+    startTimer()
+
     //* PLAYER SPACESHIP MOVEMENT ---------------------------------------------
 
     function handleKeyDown(event) {
@@ -87,29 +101,46 @@ function init() {
         case 37:
           playerPosition > 110 ? playerPosition-- : playerPosition
           break
+        case 32:
+          console.log('I am the space bar')
+          break
         default:
           playerPosition
       }
       cells[playerPosition].classList.add('spaceship')
     }
 
+    //* CREATE FUNCTION TO FIRE AT INVADERS
+ 
+    
+
+    //* event spacebar
+    //* playerposition - width the starting cell that advances up on the grid
+    //* index decrementing by width  
+    //* class is being removed from the current cell and added on to the next cell
+
+    function gameOver() {
+      gameRunning = false
+      console.log(gameRunning)
+      clearInterval(timerId)
+    }
+
+
     createGrid(playerPosition)
     createInvaders()
     document.addEventListener('keydown', handleKeyDown)
+
   }
 
-  //* CREATE FUNCTION TO FIRE AT INVADERS
-  
   //* FUNCTION TO START GAME ---------------------------------------------------
-  
+
   function handleStartBtn() {
-    //! throttle event
     gameInit()
   }
-  
+
   startBtn.addEventListener('click', handleStartBtn)
   resetBtn.addEventListener('click', gameInit)
-  
+
 }
 
 window.addEventListener('DOMContentLoaded', init)
